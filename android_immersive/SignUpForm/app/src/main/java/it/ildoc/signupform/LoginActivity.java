95ccaff7da25b721c1472ed466ebe3461f7cf74f
ildoc <filippo.giomi@gmail.com>
2016-06-07 15:48:33 +0200

    fine S09L01

--- android_immersive/SignUpForm/app/src/main/java/it/ildoc/signupform/LoginActivity.java
+++ android_immersive/SignUpForm/app/src/main/java/it/ildoc/signupform/LoginActivity.java
@@ -2,12 +2,32 @@ package it.ildoc.signupform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button submitButton;
+    EditText usernameInput;
+    EditText passwordInput;
+    EditText confirmInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        submitButton=(Button)findViewById(R.id.submit_button);
+        usernameInput = (EditText)findViewById(R.id.username_edittext);
+        passwordInput = (EditText)findViewById(R.id.password_edittext);
+        confirmInput = (EditText)findViewById(R.id.confirm_edittext);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
