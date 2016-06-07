837913ab2ad54426ea83716c3484928ffaf1d195
ildoc <filippo.giomi@gmail.com>
2016-06-07 19:32:25 +0200

    merge

--- android_immersive/MyBank/app/src/main/java/it/ildoc/mybank/MainActivity.java
+++ android_immersive/MyBank/app/src/main/java/it/ildoc/mybank/MainActivity.java
@@ -2,6 +2,7 @@ package it.ildoc.mybank;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
