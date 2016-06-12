f5afef6496826414767c03aa685244b87ca6d5fd
ildoc <filippo.giomi@gmail.com>
2016-06-12 14:27:56 +0200

    fine S11L14

--- android_immersive/TaskIt/app/src/main/java/it/ildoc/taskit/Task.java
+++ android_immersive/TaskIt/app/src/main/java/it/ildoc/taskit/Task.java
@@ -10,4 +10,31 @@ public class Task {
     private Date dueDate;
     private boolean done;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String toString(){
+        return name;
+    }
 }
--- android_immersive/TaskIt/app/src/main/java/it/ildoc/taskit/TaskListActivity.java
+++ android_immersive/TaskIt/app/src/main/java/it/ildoc/taskit/TaskListActivity.java
@@ -2,6 +2,13 @@ package it.ildoc.taskit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class TaskListActivity extends AppCompatActivity {
 
@@ -9,5 +16,43 @@ public class TaskListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
+
+        Task[] items = new Task[3];
+        items[0] = new Task();
+        items[0].setName("Task 1");
+        items[1] = new Task();
+        items[1].setName("Task 2");
+        items[1].setDone(true);
+        items[2] = new Task();
+        items[2].setName("Task 3");
+
+        ListView list = (ListView)findViewById(R.id.task_list);
+        list.setAdapter(new TaskAdapter(items));
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    private class TaskAdapter extends ArrayAdapter<Task>{
+        TaskAdapter(Task[] tasks){
+            super(TaskListActivity.this, R.layout.task_list_row, R.id.task_item_name, tasks);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+            Task task = getItem(position);
+
+            TextView taskName = (TextView)convertView.findViewById(R.id.task_item_name);
+            taskName.setText(task.getName());
+
+            CheckBox done = (CheckBox)convertView.findViewById(R.id.task_item_done);
+            done.setChecked(task.isDone());
+
+            return convertView;
+        }
     }
 }
