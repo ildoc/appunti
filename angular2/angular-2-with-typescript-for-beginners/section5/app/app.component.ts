f40afc4ee6787867d08e646697819d6a4a588dd3
ildoc <filippo.giomi@gmail.com>
2016-08-05 17:43:44 +0200

    inizio sezione 5

---
+++ angular2/angular-2-with-typescript-for-beginners/section5/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div [hidden]="courses.length == 0">
+            List of courses
+        </div>
+        <div [hidden]="courses.length > 0">
+            You don't have any courses yet.
+        </div>
+    `
+})
+export class AppComponent {
+    courses = [];
+}
