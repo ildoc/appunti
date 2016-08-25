9ead6748cdab2598b3f33fb46bca6e5e29775c64
ildoc <filippo.giomi@gmail.com>
2016-08-25 11:00:59 +0200

    fine sezione 3

---
+++ angular2/angular-2-the-complete-guide/02-recipe-book/index.ts
@@ -0,0 +1,5 @@
+export * from './/header.component';
+export * from './recipes';
+export * from './recipes/recipe-detail.component';
+export * from './shopping-list';
+export * from './shopping-list/shopping-list-add.component';
---
+++ angular2/angular-2-the-complete-guide/02-recipe-book/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListAddComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  entryComponents: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
