5c2926cd64d4417d7ba498e1a81abca67acddba9
ildoc <filippo.giomi@gmail.com>
2017-10-28 14:49:23 +0200

    progetto book-list

---
+++ react/modern_react_with_redux/book-list/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        { title: 'aaaa' },
+        { title: 'bbbb' },
+        { title: 'cccc' },
+        { title: 'dddd' },
+        { title: 'eeee' }
+    ]
+}
