21d7c8cf883bda2dd272d1101feb9908f57897de
ildoc <filippo.giomi@gmail.com>
2017-10-29 10:37:12 +0100

    inizio app meteo

--- react/modern_react_with_redux/book-list/src/reducers/reducer_books.js
+++ react/modern_react_with_redux/book-list/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
     return [
-        { title: 'aaaa' },
-        { title: 'bbbb' },
-        { title: 'cccc' },
-        { title: 'dddd' },
-        { title: 'eeee' }
+        { title: 'aaaa', pages: 101 },
+        { title: 'bbbb', pages: 40 },
+        { title: 'cccc', pages: 456 },
+        { title: 'dddd', pages: 745 },
+        { title: 'eeee', pages: 632 }
     ]
 }
