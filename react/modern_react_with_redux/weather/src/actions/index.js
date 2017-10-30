c980615189c100843b7243d0a7300e3bee49d395
ildoc <filippo.giomi@gmail.com>
2017-10-30 22:21:22 +0100

    fix

--- react/modern_react_with_redux/weather/src/actions/index.js
+++ react/modern_react_with_redux/weather/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'de6702c48a1f4d07bdae70e2d6f16a70';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
