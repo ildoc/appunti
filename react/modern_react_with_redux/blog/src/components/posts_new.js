e982e70c5b2a8cf1aa76277e3c26b5ad1ef4fe1c
ildoc <filippo.giomi@gmail.com>
2017-10-31 21:56:52 +0100

    +redux-form

---
+++ react/modern_react_with_redux/blog/src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return (
+            <div>
+                new
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
