bb7e046cd97371dbf928112174f1a7b400c0e0b8
ildoc <filippo.giomi@gmail.com>
2017-11-16 11:21:18 +0100

    firebase config

--- react-native/react-native-redux/auth/src/App.js
+++ react-native/react-native-redux/auth/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './component/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBd9QNdrlgKlTCj2_xtgfC7G5tSdAb9HgY',
+            authDomain: 'auth-a9a44.firebaseapp.com',
+            databaseURL: 'https://auth-a9a44.firebaseio.com',
+            projectId: 'auth-a9a44',
+            storageBucket: 'auth-a9a44.appspot.com',
+            messagingSenderId: '156644362918'
+        }
+        );
+    }
+
     render() {
         return (
             <View>
