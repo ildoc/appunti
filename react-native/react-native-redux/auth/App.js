cbc373cd852adca5bc1345d2caef64f3c074a34d
ildoc <filippo.giomi@gmail.com>
2017-11-20 17:53:41 +0100

    conditional rendering

--- react-native/react-native-redux/auth/App.js
+++ react-native/react-native-redux/auth/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/component/common';
+import { Header, Button, Spinner } from './src/component/common';
 import LoginForm from './src/component/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBd9QNdrlgKlTCj2_xtgfC7G5tSdAb9HgY',
@@ -13,15 +15,37 @@ class App extends Component {
       projectId: 'auth-a9a44',
       storageBucket: 'auth-a9a44.appspot.com',
       messagingSenderId: '156644362918'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log out
+        </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />
     }
-    );
   }
 
   render() {
     return (
       <View>
         <Header headerText="Auth" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
