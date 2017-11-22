8a620310b0474f4b71383969c086acdec0a733ea
ildoc <filippo.giomi@gmail.com>
2017-11-22 18:01:16 +0100

    connect function

--- react-native/react-native-redux/tech_stack/App.js
+++ react-native/react-native-redux/tech_stack/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/component/common';
 
 const App = () => {
   return (
-    <View />
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech stack" />
+      </View>
+    </Provider>
   );
 };
 
